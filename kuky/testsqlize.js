25dce81714ef0af1a48963eb7e0d906909db1dbb
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2015-11-14 17:41:38 -0500

    Fixed unknown bug

---
+++ kuky/testsqlize.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var Sequelize = require("sequelize");
+
+var sequelize = new Sequelize(process.env.database, 'root', process.env.password, {
+    host: 'localhost',
+    dialect: 'mysql',
+    
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+});
+
+var Comments = sequelize.define("Comments", {
+    ID: Sequelize.INTEGER,
+    Content: Sequelize.STRING,
+    Upvotes: Sequelize.INTEGER,
+    Downvotes: Sequelize.INTEGER
+}, {
+    classMethods: {
+        getAll: function() {
+            return 'smth';
+        }
+    },
+    instanceMethods: {
+        getComment: function() {
+            return this.Content;
+        }
+    }
+})
+
+function ShowAll() {
+    sequelize.query("select * from `Comments`", {type: sequelize.QueryTypes.SELECT})
+    .then(function(rows) {
+        rows.forEach(function(element, index, array) {
+            console.log(element.ID + ": " + element.Content);
+        })
+    })  
+}
+
+
+Comments.findById(1).then(function(comment) {
+    console.log(comment.getComment());
+})
+
