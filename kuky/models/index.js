83db71fd90adbf2c5ed69195f4732c0faca657b9
Hieu Nguyen <hieu.nguyen@students.olin.edu>
2015-11-17 00:10:14 -0500

    posts/all/recent working

--- kuky/models/index.js
+++ kuky/models/index.js
@@ -11,19 +11,19 @@ var db        = {};
 console.log(config);
 
 fs
- .readdirSync(__dirname)
- .filter(function(file) {
-   return (file.indexOf(".") !== 0) && (file !== "index.js");
- })
- .forEach(function(file) {
-   var model = sequelize.import(path.join(__dirname, file));
-   db[model.name] = model;
+    .readdirSync(__dirname)
+    .filter(function(file) {
+    return (file.indexOf(".") !== 0) && (file !== "index.js");
+    })
+    .forEach(function(file) {
+    var model = sequelize.import(path.join(__dirname, file));
+    db[model.name] = model;
  });
 
 Object.keys(db).forEach(function(modelName) {
- if ("associate" in db[modelName]) {
-   db[modelName].associate(db);
- }
+    if ("associate" in db[modelName]) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
--- kuky/routes/kus.js
+++ kuky/routes/kus.js
@@ -2,21 +2,33 @@ var express = require('express');
 var router = express.Router();
 var models  = require('../models');
 
+var Ku = models.sequelize.models.Ku;
+var Ku_user = models.sequelize.models.Ku_user;
+
 /* GET all posts in new order */
 router.get('/all/recent', function(req, res, next) {
-	models.sequelize.models.Ku.findAll({
+	Ku.findAll({
 		where: {
 			createdAt: {
 				$lt: new Date(),
-				$gt: new Date(new Date() - 24*60*60*1000)
+				$gt: new Date(new Date() - 7*24*60*60*1000)
 			}
 		}
-	}).then(function (kus){
-		kus.forEach(function (index, element, array){
-			console.log(element.dataValues.id + ": " + element.dataValues.content);
-		})
+	}).then(function(kus) {
+		kus.sort(function(a, b) {
+            return a.dataValues.createdAt - b.dataValues.createdAt;
+        })
+        res.json(kus)
 	});
-	res.json('hello')
+});
+
+router.get('/all/recent', function(req, res, next) {
+	Ku.findAll().then(function(kus) {
+		kus.sort(function(a, b) {
+            return a.dataValues.createdAt - b.dataValues.createdAt;
+        })
+        res.json(kus)
+	});
 });
 
 module.exports = router;
